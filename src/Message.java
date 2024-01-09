d590a2afe1a65e21fb36aea6589d3b1a0fcde238
Dahouede Rebson <o22203774@info27-08.iut45.univ-orleans.fr>
2024-01-09 18:28:10 +0100

    Fichier Message.java

---
+++ src/Message.java
@@ -0,0 +1,12 @@
+package src;
+import java.util.Date;
+
+public class Message {
+    private int idMessage;
+
+    private String content;
+    private Date dateDePublication;
+    private int likes;
+
+
+}
